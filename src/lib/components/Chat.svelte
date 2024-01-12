9b1faabb5ad2c2f337547418bdf595e93900d0cc
rcdonovan <rcdonovan@email.wm.edu>
2024-01-12 16:38:19 -0500

    Start conversation with intro text

--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -9,367 +9,38 @@
 
     let threadId = null;
     let threadMessage = null;
-    let allMessages = {
-        "messages": [
+    let allMessages = null;
+    let firstMessage = [
             {
                 "id": "msg_FR8UQHZaaKVupu9LLMYzlv3i",
                 "object": "thread.message",
                 "created_at": 0,
-                "thread_id": "thread_Onh9cuVkXlIr4OuThr2x0kKs",
+                "thread_id": null,
                 "role": "assistant",
                 "content": [
                     {
                         "type": "text",
                         "text": {
-                            "value": "I have access to 3.5k+ documented tickborne experiences. How can I help you?",
+                            "value": "I have access to 3.5k+ documented tickborne illness experiences. How can I help you?",
                             "annotations": []
                         }
                     }
                 ],
                 "file_ids": [],
-                "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-                "run_id": "run_xNKnXAsd7ZX1BC7cnHAxm97C",
+                "assistant_id": null,
+                "run_id": null,
                 "metadata": {}
             },
-        ]
-    };
+        ];
 
     onMount(async () => {
         if (threadId === null) {
             threadId = await getThread();
+            allMessages = [...firstMessage];
         }
     });
 
     let scrollToDiv = HTMLDivElement;
-//     let allMessages = {
-//     "messages": [
-//         {
-//             "id": "msg_bUmSEPgGEPn5EEFuNJqQGhWw",
-//             "object": "thread.message",
-//             "created_at": 1705085210,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "user",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Tell me a fact about Lyme disease",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": null,
-//             "run_id": null,
-//             "metadata": {}
-//         },
-//         {
-//             "id": "msg_rI7Yj3KcMHqOSCLQ5ebafeoY",
-//             "object": "thread.message",
-//             "created_at": 1705085226,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Lyme disease can cause psychiatric symptoms such as anxiety, depression, and depersonalization. It's important to recognize that these symptoms can be part of the disease and seek appropriate medical attention for diagnosis and treatment【2†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_lwEEPI9OkB0XEEClrzjHg4hC",
-//             "metadata": {}
-//         },
-//         {
-//             "id": "msg_7qSbAllganJ2OaUuT4cDk1un",
-//             "object": "thread.message",
-//             "created_at": 1705085255,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "user",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "tell me 3 creative treatments people are trying",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": null,
-//             "run_id": null,
-//             "metadata": {}
-//         },
-//         {
-//             "id": "msg_pbXBqUJzN1LJvX4xkDd1SImi",
-//             "object": "thread.message",
-//             "created_at": 1705085261,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Here are three creative treatments people are trying for Lyme disease:\n\n1. High-dose vitamin C, preferably liposomal or even IV infusion.\n2. Homeopathy, such as the use of DesBio.\n3. Disulfiram (Antabuse) treatment, which has been used successfully by a doctor named Horowitz, although it can be very harsh. Additionally, there is an expensive permanent treatment called SOT (supportive oligonucleotide therapy) that turns off the pathogen's reproductive gene so it stops multiplying in the system and eventually is gone. However, the downside is that only one pathogen can be targeted at a time, and for some, the immune system does not deal with the coinfections even when Lyme is gone【27†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_1Q3rDQChduMrD1mngLkNEReU",
-//             "metadata": {}
-//         },
-//         {
-//             "id": "msg_rVrSM83LF0OeZGIj2REknqM4",
-//             "object": "thread.message",
-//             "created_at": 1705085304,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "user",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "list a few antibiotics that are less common but show potential for lyme disease",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": null,
-//             "run_id": null,
-//             "metadata": {}
-//         },
-//         {
-//             "id": "msg_4ZM2ACn5ZnTgaOJOa5x3pB06",
-//             "object": "thread.message",
-//             "created_at": 1705085310,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Two less common antibiotics showing potential for Lyme disease are Dapsone and Allinia. Dapsone was prescribed due to a specific genetic Lyme marker, and the individual saw much improvement with it. Allinia is a new antibiotic that many Lyme patients are reportedly having good success with. However, the use of Allinia may depend on whether insurance covers it【32†source】【33†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_crbBzltX7PlrwLtyY2lkKAnd",
-//             "metadata": {}
-//         },
-//         {
-//             "id": "msg_8IzXvwoazUldIOdvwSCRFy6a",
-//             "object": "thread.message",
-//             "created_at": 1705085366,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "user",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "list 3 uncommon supplements that people have reported work well for them",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": null,
-//             "run_id": null,
-//             "metadata": {}
-//         },
-//         {
-//             "id": "msg_fvpeCFW8q2hDnDEaOGPCnYGM",
-//             "object": "thread.message",
-//             "created_at": 1705085373,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Here are three uncommon supplements that people have reported work well for them in the context of Lyme disease:\n\n1. Zinc, Vitamin C, Vitamin D, Vitamin K, Quercitin, NAC (N-acetyl cysteine), and glutathione, which have been recommended by individuals as helpful supplements【38†source】.\n2. Nystatin for yeast prevention, and a recommendation for a bunch of supplements, along with a note on being deficient in Vitamin D and Omega 3【40†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_3oq6l94GqSGYitU45hCEsOcY",
-//             "metadata": {}
-//         },
-// {
-//             "id": "msg_fvpeCFW8q2hDnDEaOGPCnYGM",
-//             "object": "thread.message",
-//             "created_at": 1705085374,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Here are three uncommon supplements that people have reported work well for them in the context of Lyme disease:\n\n1. Zinc, Vitamin C, Vitamin D, Vitamin K, Quercitin, NAC (N-acetyl cysteine), and glutathione, which have been recommended by individuals as helpful supplements【38†source】.\n2. Nystatin for yeast prevention, and a recommendation for a bunch of supplements, along with a note on being deficient in Vitamin D and Omega 3【40†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_3oq6l94GqSGYitU45hCEsOcY",
-//             "metadata": {}
-//         },
-// {
-//             "id": "msg_fvpeCFW8q2hDnDEaOGPCnYGM",
-//             "object": "thread.message",
-//             "created_at": 1705085375,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Here are three uncommon supplements that people have reported work well for them in the context of Lyme disease:\n\n1. Zinc, Vitamin C, Vitamin D, Vitamin K, Quercitin, NAC (N-acetyl cysteine), and glutathione, which have been recommended by individuals as helpful supplements【38†source】.\n2. Nystatin for yeast prevention, and a recommendation for a bunch of supplements, along with a note on being deficient in Vitamin D and Omega 3【40†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_3oq6l94GqSGYitU45hCEsOcY",
-//             "metadata": {}
-//         },
-// {
-//             "id": "msg_fvpeCFW8q2hDnDEaOGPCnYGM",
-//             "object": "thread.message",
-//             "created_at": 1705085376,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Here are three uncommon supplements that people have reported work well for them in the context of Lyme disease:\n\n1. Zinc, Vitamin C, Vitamin D, Vitamin K, Quercitin, NAC (N-acetyl cysteine), and glutathione, which have been recommended by individuals as helpful supplements【38†source】.\n2. Nystatin for yeast prevention, and a recommendation for a bunch of supplements, along with a note on being deficient in Vitamin D and Omega 3【40†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_3oq6l94GqSGYitU45hCEsOcY",
-//             "metadata": {}
-//         },
-// {
-//             "id": "msg_fvpeCFW8q2hDnDEaOGPCnYGM",
-//             "object": "thread.message",
-//             "created_at": 1705085377,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Here are three uncommon supplements that people have reported work well for them in the context of Lyme disease:\n\n1. Zinc, Vitamin C, Vitamin D, Vitamin K, Quercitin, NAC (N-acetyl cysteine), and glutathione, which have been recommended by individuals as helpful supplements【38†source】.\n2. Nystatin for yeast prevention, and a recommendation for a bunch of supplements, along with a note on being deficient in Vitamin D and Omega 3【40†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_3oq6l94GqSGYitU45hCEsOcY",
-//             "metadata": {}
-//         },
-// {
-//             "id": "msg_fvpeCFW8q2hDnDEaOGPCnYGM",
-//             "object": "thread.message",
-//             "created_at": 1705085378,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Here are three uncommon supplements that people have reported work well for them in the context of Lyme disease:\n\n1. Zinc, Vitamin C, Vitamin D, Vitamin K, Quercitin, NAC (N-acetyl cysteine), and glutathione, which have been recommended by individuals as helpful supplements【38†source】.\n2. Nystatin for yeast prevention, and a recommendation for a bunch of supplements, along with a note on being deficient in Vitamin D and Omega 3【40†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_3oq6l94GqSGYitU45hCEsOcY",
-//             "metadata": {}
-//         },
-// {
-//             "id": "msg_fvpeCFW8q2hDnDEaOGPCnYGM",
-//             "object": "thread.message",
-//             "created_at": 1705085378,
-//             "thread_id": "thread_atVWErIdwOvH4DwEYtNvTzJC",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Here are three uncommon supplements that people have reported work well for them in the context of Lyme disease:\n\n1. Zinc, Vitamin C, Vitamin D, Vitamin K, Quercitin, NAC (N-acetyl cysteine), and glutathione, which have been recommended by individuals as helpful supplements【38†source】.\n2. Nystatin for yeast prevention, and a recommendation for a bunch of supplements, along with a note on being deficient in Vitamin D and Omega 3【40†source】.",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_3oq6l94GqSGYitU45hCEsOcY",
-//             "metadata": {}
-//         },
-//     ]
-// };
-
-// let allMessages = {
-//     "messages": [
-//         {
-//             "id": "msg_FR8UQHZaaKVupu9LLMYzlv3i",
-//             "object": "thread.message",
-//             "created_at": 1705040773,
-//             "thread_id": "thread_Onh9cuVkXlIr4OuThr2x0kKs",
-//             "role": "assistant",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Hello! How can I assist you today?",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": "asst_3cIA9dtY4q7IqFa4OdNN21kd",
-//             "run_id": "run_xNKnXAsd7ZX1BC7cnHAxm97C",
-//             "metadata": {}
-//         },
-//         {
-//             "id": "msg_Q7EA6G9rRJEXcFkFUBQpDrlH",
-//             "object": "thread.message",
-//             "created_at": 1705040771,
-//             "thread_id": "thread_Onh9cuVkXlIr4OuThr2x0kKs",
-//             "role": "user",
-//             "content": [
-//                 {
-//                     "type": "text",
-//                     "text": {
-//                         "value": "Say hi to me!",
-//                         "annotations": []
-//                     }
-//                 }
-//             ],
-//             "file_ids": [],
-//             "assistant_id": null,
-//             "run_id": null,
-//             "metadata": {}
-//         }
-//     ]
-// };
     let runId = null;
     let runResult = null;
     let retrievedRunStatus = null;
@@ -389,7 +60,7 @@
         }
 
         threadMessage = await createMessage(threadId);
-        allMessages = await viewThread(threadId);
+        allMessages = [...await viewThread(threadId), ...firstMessage];
         loading_response = true;
 
         runResult = await createRun(threadId);
@@ -458,7 +129,7 @@
         const response = await fetch(url, requestOptions);
         const data = await response.json();
         console.log('Viewing thread:', data);
-        return data
+        return data.messages
     }
     
     async function checkAndRetrieveData() {
@@ -467,7 +138,7 @@
         if (retrievedRunStatus === 'completed') {
             clearInterval(intervalId); 
             console.log('Run status is: complete');
-            allMessages = await viewThread(threadId);
+            allMessages = [...await viewThread(threadId), ...firstMessage];
             console.log('all MESSAGES:', allMessages);
             
             loading_response = false;
@@ -498,7 +169,7 @@
 
 <div class="chatcontainer">
 {#if allMessages}
-    {#each allMessages.messages.reverse() as message}
+    {#each allMessages.reverse() as message}
         {#if message.role === 'user'}
             <div class="message chatright">
                 <p><span class="username">You<br /></span>
