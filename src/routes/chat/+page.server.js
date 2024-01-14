21cab94f650e6e0562efee7d77a6cf1e75905417
rcdonovan <rcdonovan@email.wm.edu>
2024-01-13 19:49:51 -0500

    Update auth again

--- src/routes/chat/+page.server.js
+++ src/routes/chat/+page.server.js
@@ -1,9 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
-export const load = async ({ locals }) => {
+export const load = async ({ event, locals }) => {
     // TODO: improve this check
-    if (locals.email.length <= 3){
+    if (event.locals.email.length <= 3){
         console.log("locals email", locals.email)
         throw redirect(302, "/auth")
     } else {
