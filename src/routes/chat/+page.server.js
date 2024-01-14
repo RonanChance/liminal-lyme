e675375144e4f4db0baebc8e61a2b5116da712a8
rcdonovan <rcdonovan@email.wm.edu>
2024-01-13 19:45:37 -0500

    Update chat page auth

--- src/routes/chat/+page.server.js
+++ src/routes/chat/+page.server.js
@@ -2,7 +2,8 @@ import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export const load = async ({ locals }) => {
-    if (!locals.email){
+    // TODO: improve this check
+    if (locals.email.length <= 3){
         console.log("locals email", locals.email)
         throw redirect(302, "/auth")
     } else {
