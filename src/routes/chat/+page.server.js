d5609f163b3abaac1fb78163701bee08a0d5c1d4
rcdonovan <rcdonovan@email.wm.edu>
2024-01-13 19:51:26 -0500

    Undo previous change

--- src/routes/chat/+page.server.js
+++ src/routes/chat/+page.server.js
@@ -1,9 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
-export const load = async ({ event, locals }) => {
+export const load = async ({ locals }) => {
     // TODO: improve this check
-    if (event.locals.email.length <= 3){
+    if (locals.email.length <= 3){
         console.log("locals email", locals.email)
         throw redirect(302, "/auth")
     } else {
