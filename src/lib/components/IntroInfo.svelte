56fd20679512c15954166c662a902fd458a78cd3
RonanChance <chancesec@proton.me>
2024-09-04 01:28:31 -0400

    Move page content to intro-info

--- src/lib/components/IntroInfo.svelte
+++ src/lib/components/IntroInfo.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from 'svelte';
     import { fade } from 'svelte/transition'
+    import { Popover } from 'flowbite-svelte';
     
     let animate = false;
     onMount(() => animate = true);
@@ -17,18 +18,38 @@
     const textInterval = setInterval(rotateText, interval);
 </script>
 
-<div class="intro-info pt-6 px-2">
-  <div class="rotating-text mt-10 text-center">
-    {#key currentWord}
-        <h1 in:fade|global={{duration: 750}}>YOUR <span class="highlighted-word text-center">{currentWord}</span></h1>
-    {/key}
-  </div>
 
-  {#if animate}
-    <div class="mt-8 text-center">
-      <h2 class="inline" in:fade|global={{y: 100, delay: 50, duration: 500 }}>
-          Get <span class="font-bold">insights</span> from Reddit based on <span class="font-bold">medications</span> & <span class="font-bold">supplements</span>
-      </h2>
+<div class="intro-info pt-6 px-2 pb-16">
+
+    <div class="rotating-text mt-10 text-center">
+        {#key currentWord}
+            <h1 in:fade|global={{duration: 750}}>YOUR <span class="highlighted-word text-center">{currentWord}</span></h1>
+        {/key}
     </div>
-  {/if}
+
+    {#if animate}
+        <div class="mt-8 text-center">
+        <h2 class="inline" in:fade|global={{y: 100, delay: 50, duration: 500 }}>
+            Get <span class="font-bold">insights</span> from Reddit based on <span class="font-bold">medications</span> & <span class="font-bold">supplements</span>
+        </h2>
+        </div>
+    {/if}
+
+    <div class="mt-12 text-center flex flex-wrap justify-center gap-2" in:blur={{delay: 200, duration: 600}}>
+        <a href="/search" class="whitebutton" type="button">Search Reddit</a>
+        <a href="/tree" class="whitebutton" type="button">Treatment Tree</a>
+    </div>
+
+    <h3 class="mt-12 max-w-[90%] mx-auto text-center" in:blur={{delay: 400, duration: 750}}>We collect open-source tick-borne disease data so you can see how people with your <a class="highlight" href="/about#what_conditions_are_supported">conditions</a> react to <a class="highlight" href="/about#what_supplements_are_supported">supplements</a> & <a class="highlight" href="/about#what_medications_are_supported">medications</a>
+        <button data-popover-target="popover-description" data-popover-placement="bottom-end" type="button" id="info-button">
+            <svg class="w-4 h-4 ml-0 text-gray-400 hover:text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+            </svg>
+            <span class="sr-only">Show information</span>
+        </button>
+    </h3>
+    <Popover class="w-64 text-sm font-light" triggeredBy="#info-button" data-popper-placement="left">This research is focused on tick-borne disease data from Reddit, but might expand in the future</Popover>
 </div>
+
+<style>
+</style>
--- src/lib/components/LandingPageContent.svelte
+++ src/lib/components/LandingPageContent.svelte
@@ -1,7 +1,6 @@
 <script>
     import MedicalDisclaimer from "./MedicalDisclaimer.svelte";
     import { onMount } from 'svelte';
-	import { Popover } from 'flowbite-svelte';
     import { fly, blur } from 'svelte/transition'
     import { total_scanned, total_cataloged, medications, supplements } from "./constants";
 
@@ -11,22 +10,6 @@
 
 {#if animate}
 <div class="content-container">
-    
-    <div class="mt-12 text-center flex flex-wrap justify-center gap-2" in:blur={{delay: 200, duration: 600}}>
-        <a href="/search" class="whitebutton" type="button">Search Reddit</a>
-        <a href="/tree" class="whitebutton" type="button">Treatment Tree</a>
-    </div>
-
-    <h3 class="infonote" in:blur={{delay: 400, duration: 750}}>We collect open-source tick-borne disease data so you can see how people with your <a class="highlight" href="/about#what_conditions_are_supported">conditions</a> react to <a class="highlight" href="/about#what_supplements_are_supported">supplements</a> & <a class="highlight" href="/about#what_medications_are_supported">medications</a>
-        <button data-popover-target="popover-description" data-popover-placement="bottom-end" type="button" id="info-button">
-            <svg class="w-4 h-4 ml-0 text-gray-400 hover:text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
-            </svg>
-            <span class="sr-only">Show information</span>
-        </button>
-    </h3>
-    <Popover class="w-64 text-sm font-light" triggeredBy="#info-button" data-popper-placement="left">This research is focused on tick-borne disease data from Reddit, but might expand in the future</Popover>
-
     <div class="image-content-bar" in:fly={{y:50, delay: 750, duration: 750}}>
         <div class="info-row" in:blur={{delay: 750, duration: 1250}}>
             <div class="min-w-[50%] max-w-[50%]">
@@ -121,7 +104,6 @@
     }
 
     .image-content-bar {
-        margin-top: 50px;
         background-color: var(--lightbackground);
         display: flex;
         flex-direction: row;
@@ -191,14 +173,6 @@
         color: var(--white);
         font-weight: bold;
         font-size: 50pt;
-    }
-
-    .infonote {
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 2rem;
-        max-width: 90%;
-        text-align: center;
     }
 
     .info-row:last-child {
