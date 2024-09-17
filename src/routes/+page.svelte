3363492b2242a6ceb083b48f17eba588a8540e41
RonanChance <chancesec@proton.me>
2024-09-17 13:49:39 -0400

    Refactor homepage css to tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@
     import { Popover } from 'flowbite-svelte';
     
     let animate = false;
+    let email = "";
     onMount(() => animate = true);
     
     const words = ['HEALTH', 'MEDICATION', 'JOURNEY', 'INSIGHT']
@@ -56,80 +57,84 @@
 </div>
 
 {#if animate}
-<div class="content-container">
-    <div class="image-content-bar" in:fly={{y:50, delay: 750, duration: 750}}>
-        <div class="info-row" in:blur={{delay: 750, duration: 1250}}>
+<div class="flex flex-col">
+    <div class="flex flex-col md:flex-row bg-[var(--lightbackground)] pb-6" in:fly={{y:50, delay: 750, duration: 750}}>
+        <div class="flex flex-row md:flex-col items-center pt-6 pl-[2%] pr-[2%]" in:blur={{delay: 750, duration: 1250}}>
             <div class="min-w-[50%] max-w-[50%]">
                 <img src="/bacteria_images/img1.png" alt="img1">
             </div>
-            <div class="text-info">
-                <strong>Search</strong> 
+            <div class="flex flex-col items-center text-center text-[var(--white)] text-lg max-w-[75%] px-[4%] md:pt-5 gap-1">
+                <strong class="text-2xl">Search</strong> 
                 Query our database to find tickborne illness experiences
             </div>
         </div>
         
-        <div class="info-row" in:blur={{delay: 750, duration: 1250}}>
+        <div class="flex flex-row md:flex-col items-center pt-6 pl-[2%] pr-[2%]" in:blur={{delay: 750, duration: 1250}}>
             <div class="min-w-[50%] max-w-[50%]">
                 <img src="/bacteria_images/img2.png" alt="img2">
             </div>
-            <div class="text-info">
-                <strong>Learn</strong> 
+            <div class="flex flex-col items-center text-center text-[var(--white)] text-lg max-w-[75%] px-[4%] md:pt-5 gap-1">
+                <strong class="text-2xl">Learn</strong> 
                 Find treatment ideas based on the medications and supplements that help others
             </div>
         </div>
 
-        <div class="info-row" in:blur={{delay: 750, duration: 1250}}>
+        <div class="flex flex-row md:flex-col items-center pt-6 pl-[2%] pr-[2%]" in:blur={{delay: 750, duration: 1250}}>
             <div class="min-w-[50%] max-w-[50%]">
                 <img src="/bacteria_images/img3.png" alt="img3">
             </div>
-            <div class="text-info">
-                <strong>Connect</strong>
+            <div class="flex flex-col items-center text-center text-[var(--white)] text-lg max-w-[75%] px-[4%] md:pt-5 gap-1">
+                <strong class="text-2xl">Connect</strong>
                 Find users with similar reactions and experiences
             </div>
         </div>
 
-        <div class="info-row" in:blur={{delay: 750, duration: 1250}}>
+        <div class="flex flex-row md:flex-col items-center pt-6 pb-6 md:mb-0 pl-[2%] pr-[2%]" in:blur={{delay: 750, duration: 1250}}>
             <div class="min-w-[50%] max-w-[50%]">
                 <img src="/bacteria_images/img4.png" alt="img4">
             </div>
-            <div class="text-info">
-                <strong>Return</strong>
+            <div class="flex flex-col items-center text-center text-[var(--white)] text-lg max-w-[75%] px-[4%] md:pt-5 gap-1">
+                <strong class="text-2xl">Return</strong>
                 Check back often as we add medications, supplements, and treatments
             </div>
         </div>
     
     </div>
     
-    <div class="numbersdiv" in:blur={{delay: 1000, duration: 750}}>
-        <div class="transparenttext">Scanned Comments</div>
-        <div class="numbertext">{total_scanned}</div>
+    <div class="flex flex-col items-center" in:blur={{delay: 1000, duration: 750}}>
+        <div class="mt-5 text-[var(--accent)] opacity-50 text-2xl">Scanned Comments</div>
+        <div class="text-[var(--white)] font-bold text-6xl mt-5 mb-5">{total_scanned}</div>
 
-        <hr/>
-        <div class="transparenttext">Cataloged Experiences</div>
-        <div class="numbertext">{total_cataloged}</div>
+        <hr class="opacity-50 mt-2 bg-[var(--white)] w-[80%]" />
+        <div class="mt-5 text-[var(--accent)] opacity-50 text-2xl">Cataloged Experiences</div>
+        <div class="text-[var(--white)] font-bold text-6xl mt-5 mb-5">{total_cataloged}</div>
         
-        <hr/>
-        <div class="transparenttext">Supported Medications</div>
-        <div class="numbertext">{medications.length}</div>
+        <hr class="opacity-50 mt-2 bg-[var(--white)] w-[80%]" />
+        <div class="mt-5 text-[var(--accent)] opacity-50 text-2xl">Supported Medications</div>
+        <div class="text-[var(--white)] font-bold text-6xl mt-5 mb-5">{medications.length}</div>
         
-        <hr/>
-        <div class="transparenttext">Supported Supplements</div>
-        <div class="numbertext">{supplements.length}</div>
+        <hr class="opacity-50 mt-2 bg-[var(--white)] w-[80%]" />
+        <div class="mt-5 text-[var(--accent)] opacity-50 text-2xl">Supported Supplements</div>
+        <div class="text-[var(--white)] font-bold text-6xl mt-5 mb-5">{supplements.length}</div>
     </div>
 
-    <div class="quotediv">
+    <div class="italic text-2xl sm:text-3xl font-semibold bg-[var(--white)] text-[var(--darkbackground)] px-[10%] pt-10 pb-10">
         The highest form of knowledge is empathy, for it requires us to suspend our egos and live in another's world.
         <div class="quoteattribution" style="text-align: right; font-style: normal;">
             - Plato
         </div>
     </div>
 
-    <div class="mt-12 text-center flex flex-wrap justify-center gap-2" in:blur={{delay: 200, duration: 600}}>
-        <a href="/search" class="whitebutton" type="button">Search Reddit</a>
-        <a href="/tree" class="whitebutton" type="button">Treatment Tree</a>
+    <div class="flex flex-col items-center text-center justify-center bg-[var(--lightbackground)] pt-10 pb-14 gap-4">
+        <h2 class="font-semibold">Join for Special Announcements!</h2>
+        <div class="flex gap-2 justify-center w-[90%] md:w-[65%] lg:w-[50%] xl:w-[30%] px-8">
+            <input class="w-full rounded-lg py-3 px-4 border-2 border-transparent focus:outline-none focus:ring-0 focus:border-[var(--accent)]" type="text" bind:value={email} placeholder="example@gmail.com">
+            <button class="whitebutton py-3 px-6 text-white">Join!</button>
+        </div>
+        <!-- can put link to Lymitless here eventually? -->
     </div>
     
-    <div class="disclaimer-container">
+    <div class="flex flex-col ml-auto mr-auto mt-6 max-w-[90%]">
         <MedicalDisclaimer />
     </div>
 
@@ -137,114 +142,3 @@
 {/if}
 
 <Footer />
-
-<style>
-    .quotediv {
-        padding-top: 50px;
-        padding-bottom: 50px;
-        font-style: italic;
-        font-size: 30pt;
-        font-weight: 600;
-        background-color: var(--white);
-        color: var(--darkbackground);
-
-        padding-left: 15%;
-        padding-right: 15%;
-    }
-
-    .image-content-bar {
-        background-color: var(--lightbackground);
-        display: flex;
-        flex-direction: row;
-    }
-
-    .info-row {
-        margin-top: 50px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding-left: 2%;
-        padding-right: 2%;
-    }
-
-    .text-info {
-        max-width: 75%;
-        padding-left: 4%;
-        padding-right: 4%;
-
-        display: flex;
-        flex-direction: column;
-        text-align: center;
-        color: var(--white);
-        font-size: 14pt;
-    }
-
-    strong {
-        font-size: 20pt;
-    }
-
-    .content-container {
-        display: flex;
-        flex-direction: column;
-        gap: 1.5rem;
-    }
-
-    .disclaimer-container {
-        display: flex;
-        flex-direction: column;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 50px;
-        max-width: 90%;
-    }
-
-    .numbersdiv {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-
-    hr {
-        opacity: 0.5;
-        margin-top: 20px;
-        background-color: var(--white);
-        width: 80%;
-    }
-
-    .transparenttext {
-        margin-top: 25px;
-        color:var(--accent);
-        opacity: 0.5;
-        font-size: 25pt;
-    }
-
-    .numbertext {
-        color: var(--white);
-        font-weight: bold;
-        font-size: 50pt;
-    }
-
-    .info-row:last-child {
-        margin-bottom: 50px;
-    }
-
-    @media (max-width: 768px) {
-
-        .quotediv {
-            font-size: 21pt;
-        }
-
-        .transparenttext {
-            font-size: 20pt;
-        }
-
-        .image-content-bar {
-            flex-direction: column;
-        }
-
-        .info-row {
-            flex-direction: row;
-        }
-        
-    }
-</style>
