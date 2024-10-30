9e0bfa12650badfb163cbeb9fbcc9f9e3de319cc
RonanChance <chancesec@proton.me>
2024-10-29 22:48:56 -0400

    Fetch all records if database exceeds 500 items

--- src/routes/tree/+page.js
+++ src/routes/tree/+page.js
@@ -1,7 +1,19 @@
 import PocketBase from 'pocketbase';
+
 /** @type {import('./$types').PageLoad} */
 export async function load() {
     const pb = new PocketBase('https://pb.liminallyme.com/');
-    const records = await pb.collection('tree').getFullList();
-	return { records };
+    const perPage = 500;  // set this to the max number PocketBase allows per page
+    let page = 1;
+    let allRecords = [];
+    let records;
+
+    // Fetch pages in a loop until no more records are returned
+    do {
+        records = await pb.collection('tree').getList(page, perPage, { skipTotal: true });
+        allRecords = allRecords.concat(records.items);
+        page++;
+    } while (records.items.length === perPage);
+
+    return { records: allRecords };
 }
